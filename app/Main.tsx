20c1706a3ddfa3c7208d86c109f36a4bea397f58
Jesus Herman-Marina <jherman@suse.com>
2023-11-06 12:54:43 +0100

    Remove image opt

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="my-6 flex flex-col items-center gap-x-12 xl:mb-12 xl:flex-row">
         <div className="mr-8 pt-6">
           <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, I’m Jesus Herman
+            !Hola!, Soy Jesús Herman Marina
           </h1>
           <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
             {`¡Hola! Soy Jesús Herman y te doy la bienvenida a ${siteMetadata.description}. Con formación en medios digitales y una larga trayectoria como diseñador web y especialista en UX, actualmente soy Project Manager en SUSE, una destacada empresa de Open Source. En mi tiempo libre, disfruto creando y
