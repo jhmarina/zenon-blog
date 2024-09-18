b1a9c89a848c1fef35623d175e7fe5a3ebf842ea
Jesus Herman Marina <jhmarina@gmail.com>
2024-09-18 15:10:55 +0200

    Arreglamos problema con URLs en export

--- next.config.js
+++ next.config.js
@@ -62,6 +62,7 @@ module.exports = () => {
   return plugins.reduce((acc, next) => next(acc), {
     output: "export",
     reactStrictMode: true,
+    exportTrailingSlash: true,
     pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
     eslint: {
       dirs: ["app", "components", "layouts", "scripts"],
